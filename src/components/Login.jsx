3008ccc8a3bc053bde5429714883006ee03ffe0b
Justin Lee <justinhylee135@gmail.com>
2023-08-19 02:49:02 -0400

    update login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { signInWithGoogle } from '../firebase';
-import { useAuth } from '../AuthContext.jsx';
-import { getFirestore, doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithGoogle } from "../firebase";
+import { useAuth } from "../AuthContext.jsx";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 function Login() {
   const { currentUser, setCurrentUser } = useAuth();
   const navigate = useNavigate();
   const db = getFirestore();
+  const auth = getAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false); // New state to toggle between Sign In and Sign Up
 
   const handleGoogleSignIn = async () => {
     const user = await signInWithGoogle();
@@ -16,7 +27,7 @@ function Login() {
       setCurrentUser(user);
 
       // Check if user exists in Firestore
-      const userRef = doc(db, 'users', user.uid);
+      const userRef = doc(db, "users", user.uid);
       const userSnap = await getDoc(userRef);
 
       if (!userSnap.exists()) {
@@ -33,10 +44,56 @@ function Login() {
         // For this example, I'm just leaving it as is
       }
 
-      navigate('/'); // Navigate to the home page
-
+      navigate("/"); // Navigate to the home page
     } else {
       console.log("Failed to sign in with Google");
+    }
+  };
+
+  const handleEmailSignIn = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      setCurrentUser(user);
+      navigate("/"); // Navigate to the home page
+    } catch (err) {
+      setError("Failed to sign in: " + err.message);
+    }
+  };
+
+  const handleEmailSignUp = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      setCurrentUser(user);
+
+      // Create a new user document in Firestore
+      const userRef = doc(db, "users", user.uid);
+      await setDoc(userRef, {
+        uid: user.uid,
+        name: user.displayName || "",
+        email: user.email,
+        profileImage: user.photoURL || "",
+        plants: 0, // Initial value
+      });
+
+      navigate("/"); // Navigate to the home page
+    } catch (err) {
+      setError("Failed to sign up: " + err.message);
     }
   };
 
@@ -46,6 +103,51 @@ function Login() {
 
   return (
     <div>
+      <form onSubmit={handleEmailSignIn}>
+        <div>
+          <label>Email: </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+
+      <form onSubmit={handleEmailSignUp}>
+        <div>
+          <label>Email: </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+
+      {error && <p>{error}</p>}
       <button onClick={handleGoogleSignIn}>Sign in with Google</button>
     </div>
   );
